23e775fd02b95aa4e0ef63b72b823a571e95d758
Martin FC <martinfcardone@gmail.com>
2024-06-22 13:58:32 -0300

    Se quito el console log de la suma total

--- js/dom.js
+++ js/dom.js
@@ -182,7 +182,7 @@ preciofinal.innerText= ("Precio Aproximado:") + sumatotal + ("€")
 letrachica.innerText= ("El precio es estimativo y en condiciones ideales de reforma. Incluye instalación de cerámicas de suelo y paredes")
 alert ("Precio Estimativo final es " + sumatotal + "€")          
 console.log (totalelectro)
-console.log (sumatotal)
+
         
             
         
